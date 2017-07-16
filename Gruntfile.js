fb664d2f06c1c874137f2e401b293595176eb8c6
rakesh <rak1994kumar@gmail.com>
2017-07-16 22:17:23 +0530

    dasda

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,40 @@
 var mkdirp = require('mkdirp');
 module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
 
+    mkdirp('public_dev', function (err) {
+    if (err) throw new Error("Error in Creation of public_dev"+err);
+    else 
+        console.log('created Successfully');
+    });
 
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
         watch: {
             set1: {
-                files: ['public/css/**/*.css'],
+                files: ['public_dev/css/**/*.css'],
                 tasks: ['cssmin']
             },
             set2: {
-                files: ['public/js/**/*.js'],
+                files: ['public_dev/js/**/*.js'],
                 tasks: ['uglify']
             }
         },
         cssmin: {
             dev: {
-                files: [{ 'public/css/main.min.css': 'public/css/main.css' }]
+                            // destination              //source
+                files: [{ 'public/css/main.min.css': 'public_dev/css/main.css' }]
             }
         },
 
         uglify: {
             dev: {
-                files: {
-                    'public/js/all.min.js': ['public/js/**/3.js', 'public/js/**/2.js', 'public/js/**/1.js'],
-                    expand: true,
-                    flatten: false,
-                }
+                files: [{
+                     // destination              //source
+                    'public/js/all.min.js': ['public_dev/js/**/1.js','public_dev/js/**/2.js'],
+                     expand: false,
+                     flatten: false,
+                }]
 
             }
         },
@@ -36,14 +44,20 @@ module.exports = function (grunt) {
                 bsFiles: {
                     src: [
                         'public/css/*.css',
-                        'public/js/*.js',
                         'public/*.html',
                         'public/ng/**/*'
                     ]
                 },
                 options: {
                     watchTask: true,
-                    server: './public'
+                    notify: false,
+                    open: true,  //  automatic open the index.html files in browser
+                    reload: true,
+                    spawn: true,
+                    server: {
+                        baseDir: "./public"
+                    }
+
                 }
             }
         }
@@ -55,7 +69,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-css');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     // grunt.registerTask('default', 'watch:set1', 'watch:set2');
-    grunt.registerTask('default', ['browserSync', 'watch:set1', 'watch:set2']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
 };
---
+++ public_dev/js/1.js
@@ -0,0 +1,5 @@
+
+
+function add(a,b){
+    console.log("DSDADS");
+}
