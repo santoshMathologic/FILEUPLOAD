2e51cb6ab49b2636b272d78d58323f58fcffb62b
rakesh <rak1994kumar@gmail.com>
2017-07-16 22:51:25 +0530

    dasdas

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,13 @@
+" use strict";
+
 var mkdirp = require('mkdirp');
 module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
 
     mkdirp('public_dev', function (err) {
-    if (err) throw new Error("Error in Creation of public_dev"+err);
-    else 
-        console.log('created Successfully');
+        if (err) throw new Error("Error in Creation of public_dev" + err);
+        else
+            console.log('created Successfully');
     });
 
     grunt.initConfig({
@@ -18,22 +20,40 @@ module.exports = function (grunt) {
             set2: {
                 files: ['public_dev/js/**/*.js'],
                 tasks: ['uglify']
+            },
+            set3: {
+                files: ['public_dev/**/'],
+                tasks: ['processhtml']
             }
         },
         cssmin: {
             dev: {
-                            // destination              //source
+                // destination              //source
                 files: [{ 'public/css/main.min.css': 'public_dev/css/main.css' }]
+            }
+        },
+        ngmin: {
+            dev: {
+                // destination            //source
+                files: [{ 'public/**/': 'public_dev/**/*.html' }]
+            }
+        },
+
+        processhtml: {
+            dist: {
+                files: {
+                    'public/index.html': ['public_dev/index.html']
+                }
             }
         },
 
         uglify: {
             dev: {
                 files: [{
-                     // destination              //source
-                    'public/js/all.min.js': ['public_dev/js/**/1.js','public_dev/js/**/2.js'],
-                     expand: false,
-                     flatten: false,
+                    // destination              //source
+                    'public/js/all.min.js': ['public_dev/js/**/1.js', 'public_dev/js/**/2.js'],
+                    expand: false,
+                    flatten: false,
                 }]
 
             }
@@ -43,9 +63,9 @@ module.exports = function (grunt) {
             dev: {
                 bsFiles: {
                     src: [
-                        'public/css/*.css',
-                        'public/*.html',
-                        'public/ng/**/*'
+                        'public_dev/css/*.css',
+                        'public_dev/*.html',
+                        'public_dev/ng/**/*'
                     ]
                 },
                 options: {
@@ -60,7 +80,13 @@ module.exports = function (grunt) {
 
                 }
             }
-        }
+        },
+        env: {
+            options: {},
+            dev: { NODE_ENV: 'DEVELOPMENT' },
+            prod: { NODE_ENV: 'PRODUCTION' }
+
+        },
     });
 
     grunt.loadNpmTasks('grunt-contrib-cssmin');
@@ -69,6 +95,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-css');
+    grunt.loadNpmTasks('grunt-processhtml');
     grunt.loadNpmTasks('grunt-contrib-clean');
 
     // grunt.registerTask('default', 'watch:set1', 'watch:set2');
